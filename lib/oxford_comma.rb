c2ae5e5eee783c9d470c4ae85f5bb8306d0dc6ad
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-26 16:22:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,19 @@
-def oxford_comma(array)
-array = ["kiwi"]
-array = array.join("and")
+#def oxford_comma(array)
+#array = ["kiwi"]
+#array = array.join("and")
+#end
+
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array = array.join("") 
+  array.insert(-7," and ")
+elsif array.length == 3
+  array = array.join(", ") 
+  array.insert(-10,"and ")
+elsif array.length == 5
+  array = array.join(", ") 
+  array.insert(-14, "and ")
 end
 
 
