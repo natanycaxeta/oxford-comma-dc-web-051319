788ed0936b401f6643bfc57c31505cb3842dba58
Natany Caxeta <natanycaxeta@icloud.com>
2019-04-26 16:29:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,6 @@
-def oxford_comma(array)
-if array.length == 1
-  array.join
-elsif array.length == 2
-  array = array.join("") 
-  array.insert(-7," and ")
-elsif array.length == 3
-  array = array.join(", ") 
-  array.insert(-10,"and ")
-elsif array.length == 5
-  array = array.join(", ") 
-  array.insert(-14, "and ")
-end
-end
 
+def oxford_comma(array)
+return array.first if array.size == 1
  if array.size == 2
     array.insert(1, "and").join(" ")
   elsif array.size > 2
